9e36a428f75748e829a355f3b996b5fd7627e4b7
DuGuYifei <liuyifei97@163.com>
2023-03-27 13:39:28 +0200

    test commit from my test branch yifei

--- src/main/java/org/sso/Main.java
+++ src/main/java/org/sso/Main.java
@@ -3,5 +3,6 @@ package org.sso;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello its Yifei here.");
     }
 }
