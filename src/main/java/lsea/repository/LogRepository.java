56c4da8b68141347dce3daf0282c334d0f2aabbb
Tomasz Dabrowski <tomasz.dabrowski@carrier.com>
2023-05-16 12:29:29 +0200

    #41 Remove user id from retrieving logs

--- src/main/java/lsea/repository/LogRepository.java
+++ src/main/java/lsea/repository/LogRepository.java
@@ -15,24 +15,21 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface LogRepository extends JpaRepository<Log, UUID> {
+  /**
+   * Find all logs for a given user.
+   *
+   * @param pageable - the page
+   * @return a list of logs for the user
+   */
+  @Query("SELECT l FROM Log l ORDER BY l.createdAt DESC")
+  List<Log> findLogs(Pageable pageable);
 
-    /**
-     * Find all logs for a given user.
-     *
-     * @param userId - the user's id
-     * @param pageable - the page
-     * @return a list of logs for the user
-     */
-    @Query("SELECT l FROM Log l WHERE l.userId = ?1 ORDER BY l.createdAt DESC")
-    List<Log> findUserLogs(UUID userId, Pageable pageable);
-
-    /**
-     * Find all logs for a given user that were created after a given timestamp.
-     *
-     * @param userId - the user's id
-     * @param timestamp - the timestamp
-     * @return a list of logs for the user
-     */
-    @Query("SELECT l FROM Log l WHERE l.userId = ?1 AND l.createdAt > ?2 ORDER BY l.createdAt DESC")
-    List<Log> findUserLiveLogs(UUID userId, Timestamp timestamp);
+  /**
+   * Find all logs for a given user that were created after a given timestamp.
+   *
+   * @param timestamp - the timestamp
+   * @return a list of logs for the user
+   */
+  @Query("SELECT l FROM Log l WHERE l.createdAt > ?1 ORDER BY l.createdAt DESC")
+  List<Log> findLiveLogs(Timestamp timestamp);
 }
