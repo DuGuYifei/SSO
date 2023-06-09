59d398df6fd296a8f8cb94a8175dc7e10cef8d38
DuGuYifei <liuyifei97@163.com>
2023-06-09 16:30:33 +0200

    #62 Add tests based on coverage report

--- src/test/java/lsea/controllers/ManagementControllerTest.java
+++ src/test/java/lsea/controllers/ManagementControllerTest.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 
 import lsea.LaboratoryApplication;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
@@ -328,6 +329,131 @@ public class ManagementControllerTest {
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    /**
+     * This test method test successfully send a post request to the "/api/v1/management/report"
+     *
+     * @throws Exception if the test fails
+     */
+    @Test
+    @DisplayName("Test of ManagementController report - isOK")
+    @Rollback
+    public void testGenerateReportIsOK() throws Exception {
+        // Arrange
+        generateTestData();
+
+        String userAuthRequest = "{\n" +
+                "    \"password\": \"test_admin\",\n" +
+                "    \"email\": \"test_admin@example.com\"\n" +
+                "}";
+
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/api/v1/users/authorize")
+                        .contentType("application/json;charset=UTF-8")
+                        .content(userAuthRequest))
+                .andReturn();
+
+        Cookie cookie = result.getResponse().getCookie("token");
+
+        String request = "{\n" +
+                "    \"numThreads\": 5,\n" +
+                "    \"iterations\": 1\n" +
+                "}";
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/v1/management/report")
+                .cookie(cookie)
+                .contentType("application/json;charset=UTF-8")
+                .content(request);
+
+        // Act
+        result = mockMvc.perform(requestBuilder)
+                .andReturn();
+
+        int status = result.getResponse().getStatus();
+        byte[] content = result.getResponse().getContentAsByteArray();
+
+        // Assert
+        Assert.assertEquals(status, 200);
+        // xlsx file
+        Assert.assertEquals(content[0], (byte) 0x50);
+        Assert.assertEquals(content[1], (byte) 0x4B);
+        Assert.assertEquals(content[2], (byte) 0x03);
+        Assert.assertEquals(content[3], (byte) 0x04);
+    }
+
+    @Test
+    @DisplayName("Test of ManagementController report - no token in cookie")
+    @Rollback
+    public void testGenerateReportNoToken() throws Exception {
+        // Arrange
+        String request = "{\n" +
+                "    \"numThreads\": 5,\n" +
+                "    \"iterations\": 1\n" +
+                "}";
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/v1/management/report")
+                .cookie(new Cookie("notoken", "test"))
+                .contentType("application/json;charset=UTF-8")
+                .content(request);
+
+        // Act
+        mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isForbidden())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("A validation error occured: No token found in cookies"));
+    }
+
+    /**
+     * This test method test successfully send a get request to the "/api/v1/management/perform-test-on-database"
+     *
+     * @throws Exception if the test fails
+     */
+    @Test
+    @DisplayName("Test of ManagementController database test report")
+    @Rollback
+    public void testDatabaseReport() throws Exception {
+        // Arrange
+        String userAuthRequest = "{\n" +
+                "    \"password\": \"test_admin\",\n" +
+                "    \"email\": \"test_admin@example.com\"\n" +
+                "}";
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/api/v1/users/authorize")
+                        .contentType("application/json;charset=UTF-8")
+                        .content(userAuthRequest))
+                .andReturn();
+
+        Cookie cookie = result.getResponse().getCookie("token");
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/v1/management/perform-test-on-database")
+                .cookie(cookie)
+                .param("logsNumber", "1");
+
+        // Act
+        result = mockMvc.perform(requestBuilder)
+                .andReturn();
+
+        // Assert
+        Assert.assertEquals(result.getResponse().getStatus(), 200);
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("totalDurationWrite"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("totalDurationRead"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("averageDurationWrite"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("averageDurationRead"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("medianDurationWrite"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("medianDurationRead"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("standardDeviationWrite"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("standardDeviationRead"));
+        Assert.assertTrue(result.getResponse().getContentAsString().contains("writeMemoryUsage"));
+    }
+
+    /**
+     * Generate data used for testing
+     *
+     * @throws Exception Exception of mockMvc.perform
+     */
     private void generateTestData() throws Exception {
         String userRequest = "{\n" +
                 "    \"username\": \"test\",\n" +
